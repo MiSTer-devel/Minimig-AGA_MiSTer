971498e12318d2944dbb5c09cefc3e1b3ed27964
Rok Krajnc <rok.krajnc@gmail.com>
2011-12-29 22:36:32 +0100

    copper module copied verbatim from minimig sources.

--- rtl/minimig/Copper.v
+++ rtl/minimig/Copper.v
@@ -51,25 +51,29 @@
 // 2009-01-16	- clean-up
 // 2009-05-24	- clean-up & renaming
 // 2009-05-29	- dma_bpl replaced with dma_ena
+// 2009-10-07	- implementation of blocked $E1 cycles
+//				- modified copper restart
+// 2010-06-16	- ECS/OCS CDANG behaviour implemented
 
 module copper
 (
-	input 	clk,	 					//bus clock
-	input 	reset,	 					//reset
-	output	reqdma,						//copper requests dma cycle
-	input	ackdma,						//agnus dma priority logic grants dma cycle
-	input	enadma,						//current slot is not used by any higher priority DMA channel
-	input	sof,						//start of frame input
-	input	bbusy,						//blitter busy flag input
-	input	[7:0] vpos,					//vertical beam counter
-	input	[8:0] hpos,
-	input 	[15:0] data_in,	    		//bus data in
-	input 	[8:1] reg_address_in,		//register address inputs
-	output 	reg [8:1] reg_address_out,	//register address outputs
-	output 	reg [20:1] address_out 		//chip address outputs
+	input 	clk,	 					// bus clock
+	input 	reset,	 					// system reset (synchronous)
+	input	ecs,						// enable ECS chipset features
+	output	reqdma,						// copper requests dma cycle
+	input	ackdma,						// agnus dma priority logic grants dma cycle
+	input	enadma,						// current slot is not used by any higher priority DMA channel
+	input	sof,						// start of frame input
+	input	blit_busy,					// blitter busy flag input
+	input	[7:0] vpos,					// vertical beam counter
+	input	[8:0] hpos,					// horizontal beam counter
+	input 	[15:0] data_in,	    		// data bus input
+	input 	[8:1] reg_address_in,		// register address input
+	output 	reg [8:1] reg_address_out,	// register address output
+	output 	reg [20:1] address_out 		// chip address output
 );
 
-//register names and adresses		
+// register names and adresses		
 parameter COP1LCH = 9'h080;
 parameter COP1LCL = 9'h082;
 parameter COP2LCH = 9'h084;
@@ -79,38 +83,55 @@ parameter COPINS  = 9'h08c;
 parameter COPJMP1 = 9'h088;
 parameter COPJMP2 = 9'h08a;
 
-//local signals
-reg		[20:16] cop1lch;	//copper location register 1
-reg		[15:1] cop1lcl;		//copper location register 1
-reg		[20:16] cop2lch;	//copper location register 2
-reg		[15:1] cop2lcl;		//copper location register 2
-reg		cdang;				//copper danger bit
-reg		[15:1] ir1;			//instruction register 1
-reg		[15:0] ir2;			//instruction register 2
-reg		[2:0] copper_state;	//current state of copper state machine
-reg		[2:0] copper_next;	//next state of copper state machine
+// copper states
+parameter RESET     = 3'b000;
+parameter FETCH1    = 3'b100;
+parameter FETCH2    = 3'b101;
+parameter WAITSKIP1 = 3'b111;
+parameter WAITSKIP2 = 3'b110;
 
-reg		strobe1;			//strobe 1 
-reg		strobe2;			//strobe 2 
-reg		beammatch;			//true if beamcounters >= value in instruction register
-reg		illegalreg;			//illegal register (MOVE instruction)
-reg		skip_flag;			//skip move instruction latch
-reg		selins;				//load instruction register (register address out = COPINS)
-reg		selreg;				//load chip register address, when both selins and selreg are active
-							//a dummy cycle is executed
-reg		skip;				//skip next move instruction (input to skip_flag register)
-reg		dmaena;				//indicates CCK cycle where copper is active
-wire	enable;				//enables copper fsm and dma slot
+// local signals
+reg		[20:16] cop1lch;	// copper location register 1
+reg		[15:1] cop1lcl;		// copper location register 1
+reg		[20:16] cop2lch;	// copper location register 2
+reg		[15:1] cop2lcl;		// copper location register 2
+reg		cdang;				// copper danger bit
+reg		[15:1] ir1;			// instruction register 1
+reg		[15:0] ir2;			// instruction register 2
+reg		[2:0] copper_state;	// current state of copper state machine
+reg		[2:0] copper_next;	// next state of copper state machine
+
+reg		strobe1;			// strobe 1 
+reg		strobe2;			// strobe 2 
+reg		strobe;
+reg		illegalreg;			// illegal register (MOVE instruction)
+reg		skip_flag;			// skip move instruction latch
+reg		selins;				// load instruction register (register address out = COPINS)
+reg		selreg;				// load chip register address, when both selins and selreg are active
+							// a dummy cycle is executed
+reg		skip;				// skip next move instruction (input to skip_flag register)
+
+wire	enable;				// enables copper fsm and dma slot
 reg		dma_req;
 wire	dma_ack;
-reg		t_beam_match;
-reg		beam_match;			//delayed beam match signal by 4 CLKs
+reg		beam_match;			// delayed beam match signal
+wire	beam_match_skip;	// beam match signal for SKIP condition check
+reg		beam_match_wait;	// beam match signal for WAIT condition chaeck
+
+wire	clk_ena;			// enables every other clock cycle for chipset use 
+reg		bus_ena;			// enables CCK cycle for copper use
+reg		bus_blk;			// bus blocked by attempting an access in the "unusable" cycle
 
 //--------------------------------------------------------------------------------------
 
-//local horizontal counter - to compensate for WAIT instruction delay it is advanced 4 lores pixels
-//the last cycle in line is not usable by copper so the max hpos visible to copper is $E2
-//always @(posedge clk)
+// since Minimig's memory bus runs twice as fast as its real Amiga counterpart
+// the chipset is required to use every other memory cycle to run virtually at the same speed
+assign clk_ena = hpos[0];
+
+// horizontal counter in Agnus is advanced 4 lores pixels in comparision with the one in Denise
+// if the horizontal line contains odd number of CCK cycles (short lines of NTSC mode and all lines of PAL mode)
+// there is a place where two cycles usable by the copper are located back to back
+// in such a situation the first cycle is not used (but locks the bus if it has a chance)
 
 //write copper location register 1 high and low word
 always @(posedge clk)
@@ -157,52 +178,73 @@ always @(posedge clk)
 
 //chip address pointer (or copper program counter) controller
 always @(posedge clk)
-	if (strobe1 || sof)//load pointer with location register 1
+	if (dma_ack && strobe1 && copper_state==RESET)//load pointer with location register 1
 		address_out[20:1] <= {cop1lch[20:16],cop1lcl[15:1]};
-	else if (strobe2)//load pointer with location register 2
+	else if (dma_ack && strobe2 && copper_state==RESET)//load pointer with location register 2
 		address_out[20:1] <= {cop2lch[20:16],cop2lcl[15:1]};
-	else if (ackdma && (selins || selreg))//increment address pointer (when not dummy cycle) 
+	else if (dma_ack && (selins || selreg))//increment address pointer (when not dummy cycle) 
 		address_out[20:1] <= address_out[20:1] + 1;
 
 //--------------------------------------------------------------------------------------
 
-//regaddress output select
-//if selins=1 the address of the copper instruction register
-//is sent out (not strictly necessary as we can load copins directly. However, this is 
-//more according to what happens in a real amiga... I think), else the contents of
-//ir2[8:1] is selected 
-//(if you ask yourself: IR2? is this a bug? then check how ir1/ir2 are loaded in this design)
+// regaddress output select
+// if selins=1 the address of the copper instruction register
+// is sent out (not strictly necessary as we can load copins directly. However, this is 
+// more according to what happens in a real amiga... I think), else the contents of
+// ir2[8:1] is selected 
+// (if you ask yourself: IR2? is this a bug? then check how ir1/ir2 are loaded in this design)
 always @(selins or selreg or ir2)
-	if (selins && !selreg) //load our instruction register
+	if (enable & selins) //load our instruction register
 		reg_address_out[8:1] = COPINS[8:1];
-	else if (selreg && !selins)//load register in move instruction
+	else if (enable & selreg)//load register in move instruction
 		reg_address_out[8:1] = ir2[8:1];
 	else
 		reg_address_out[8:1] = 8'hFF;//during dummy cycle null register address is present
 
-//detect illegal register access
-always @(ir2 or cdang)
-	if((ir2[8:7] == 2'b00) && (cdang==0))//$000 -> $07E illegal if cdang=0
-		illegalreg = 1;
-	else//$080 -> $1FE always allowed
-		illegalreg = 0;
+// detect illegal register access
+// CDANG = 0 (OCS/ECS) : $080-$1FE allowed
+// CDANG = 1 (OCS)     : $040-$1FE allowed
+// CDANG = 1 (ECS)     : $000-$1FE allowed
+always @(ir2 or cdang or ecs)
+	if (ir2[8:7]==2'b00 && !cdang || ir2[8:6]==3'b000 && !ecs) // illegal access
+		illegalreg = 1'b1;
+	else // $080 -> $1FE always allowed
+		illegalreg = 1'b0;
 
 //--------------------------------------------------------------------------------------
 
-//strobe1 (also triggered by sof, start of frame)
-always @(reg_address_in)
-	if (reg_address_in[8:1]==COPJMP1[8:1])
-		strobe1 = 1;
-	else
-		strobe1 = 0;
-
-//strobe2
-always @(reg_address_in)
+reg copjmp1, copjmp2;
+	
+always @(posedge clk)
+	if (reg_address_in[8:1]==COPJMP1[8:1] || sof)
+		copjmp1 = 1;
+	else if (clk_ena)
+		copjmp1 = 0;
+		
+always @(posedge clk)
 	if (reg_address_in[8:1]==COPJMP2[8:1])
+		copjmp2 = 1;
+	else if (clk_ena)
+		copjmp2 = 0;
+		
+//strobe1 (also triggered by sof, start of frame)
+always @(posedge clk)
+	if (copjmp1 && clk_ena)
+		strobe1 = 1;
+	else if (copper_state==RESET && dma_ack)
+		strobe1 = 0;
+		
+//strobe2
+always @(posedge clk)
+	if (copjmp2 && clk_ena)
 		strobe2 = 1;
-	else
+	else if (copper_state==RESET && dma_ack)
 		strobe2 = 0;
-
+		
+always @(posedge clk)
+	if (clk_ena)
+		strobe = copjmp1 | copjmp2;		
+		
 //--------------------------------------------------------------------------------------
 
 //beam compare circuitry
@@ -233,20 +275,19 @@ assign vercmp[5] = (ir2[13]) ? ir1[13] : vpos[5];
 assign vercmp[6] = (ir2[14]) ? ir1[14] : vpos[6];
 assign vercmp[7] = ir1[15];
  
-//final beamcounter compare logic 
-//(also takes bbusy/blitter finished disable into account)
-always @(hpos or vpos or vercmp or horcmp or ir2[15] or bbusy)
-	if ({vpos[7:0],hpos[8:2]}>={vercmp[7:0],horcmp[8:2]})
-	begin
-		if (ir2[15])//blitter finished disabled
-			beammatch = 1;
-		else if (!bbusy)//blitter is finished
-			beammatch = 1;
-		else//blitter not finished yet
-			beammatch = 0;
-	end
-	else
-		beammatch = 0;
+// actual beam position comparator
+always @(posedge clk)
+	if (clk_ena)
+		if ({vpos[7:0],hpos[8:2]} >= {vercmp[7:0],horcmp[8:2]}) 
+			beam_match <= 1'b1;
+		else
+			beam_match <= 1'b0;
+
+assign beam_match_skip = beam_match & (ir2[15] | ~blit_busy);
+
+always @(posedge clk)
+	if (clk_ena)
+		beam_match_wait <= beam_match_skip;
 
 //--------------------------------------------------------------------------------------
 /*
@@ -256,63 +297,53 @@ WAIT: first cycle after fetch of second instruction word is a cycle when compari
 this comparision is beeing done all the time regardless of the available DMA slot
 when the comparision condition is safisfied the FSM goes to wait_wake_up state,
 it stays in this state as long as display DMA takes the DMA slots
-when display DMA doesn't use even bus cycle the FMS advances to the fetch state (the slot isn't used by the copper, DBR is deasserted)
+when display DMA doesn't use even bus cycle the FSM advances to the fetch state (the slot isn't used by the copper, DBR is deasserted)
 such a behaviour is caused by dma request pipelining in real Agnus
 
 */
 //--------------------------------------------------------------------------------------
     
-//copper states
-parameter RESET1    = 3'b000;
-parameter RESET2    = 3'b001;
-parameter FETCH1    = 3'b101;
-parameter FETCH2    = 3'b111;
-parameter WAITSKIP1 = 3'b110;
-parameter WAITSKIP2 = 3'b100;
-
-//generate request dma (reqdma) signal			 
-//for a dma to be requested first of all the cycle must be right (hpos[1:0])
-//(copper only uses even cycles: hpos[1:0]==2'b01)
-//second, selins or selreg must be true (state machine request bus operation)
-//the last cycle in a line is not usable by the copper (see AHRM)
-//When copper wants to access memory in E1 the DBR is activated in E1 and E2 and it seems to be one cycle 
-//(it holds the blitter and CPU off the bus during that time)
+//generate dma request signal (reqdma)
+//copper only uses even cycles: hpos[1:0]==2'b01)
+//the last cycle of the short line is not usable by the copper
+//in PAL mode when the copper wants to access memory bus in cycle $E1 the DBR is activated 
+//(blocks the blitter and CPU) but actual transfer takes place in the next cycle (DBR still asserted)
 
 always @(posedge clk)
-	if (hpos[0])
+	if (clk_ena)
 		if (hpos[8:1]==8'h01)
-			dmaena <= 0;
+			bus_blk <= 1; //cycle $E1 is blocked
 		else
-			dmaena <= ~dmaena;
-			
-assign enable = dmaena & hpos[0];
+			bus_blk <= 0;
 
-assign reqdma = dma_req & enable;
-assign dma_ack = ackdma;
+always @(posedge clk)
+	if (clk_ena)
+		if (bus_blk)
+			bus_ena <= 1; //cycle $E2 is usable
+		else
+			bus_ena <= ~bus_ena;
+						
+assign enable = ~bus_blk & bus_ena & clk_ena;
+
+assign reqdma = dma_req & bus_ena & clk_ena; //dma is request also during $E1 but output register address is idle
+assign dma_ack = ackdma & enable; //dma ack is masked during $E1
 assign dma_ena = enadma; //dma slot is empty and can be used by copper
 
-always @(posedge clk)
-	if (hpos[0])
-	begin
-		t_beam_match <= beammatch;
-		beam_match <= t_beam_match;
-	end
-	
+
+//hint: during vblank copper instruction pointer is reloaded just after the first refresh slot	
+//there is at least 2 CCK delay between writing COPJMPx register and pointer reload 
 //copper state machine and skip_flag latch
 always @(posedge clk)
-	if (sof)
-		copper_state <= RESET1;
-	else if (reset || strobe1 || strobe2)//on strobe or reset fetch first instruction word
-		copper_state <= RESET1;
-	//else if (ackdma || (copper_state==RESET1 && dmaena && hpos[0]))//if granted dma cycle go to next state
-	else if (enable) //go to next state
+	if (reset || clk_ena && strobe) // on strobe or reset fetch first instruction word
+		copper_state <= RESET;
+	else if (enable) // go to next state
 		copper_state <= copper_next;
 
 always @(posedge clk)
-	if (enable)//if granted dma cycle go to next state
+	if (enable)
 		skip_flag <= skip;
 	
-always @(copper_state or ir2 or beam_match or illegalreg or skip_flag or dma_ack or dma_ena)
+always @(copper_state or ir2 or beam_match_wait or beam_match_skip or illegalreg or skip_flag or dma_ack or dma_ena)
 begin
 	case (copper_state)
 	
@@ -320,27 +351,17 @@ begin
 		//usually first cycle is a read of the next instruction to COPINS or bitplane DMA,
 		//the second is dma free cycle (it's a dummy cycle requested by copper but not used to transfer data)
 		
-		//even cycle without dma slot
-		RESET1:
+		//after reset or strobe write an allocated DMA cycle is required to reload instruction pointer from location registers
+		RESET:
 		begin
 			skip = 0;
 			selins = 0;
 			selreg = 0;
-			dma_req = 0;
-			copper_next = RESET2;
-		end
-		
-		//after reset or strobe write allocated DMA cycle is required to reload instruction pointer from location registers
-		RESET2:
-		begin
-			skip = 0;
-			selins = 0;
-			selreg = 0;
-			dma_req = 1;
+			dma_req = 1; //a DMA access is requested to reload instuction pointer
 			if (dma_ack)
 				copper_next = FETCH1;
 			else
-				copper_next = RESET2;
+				copper_next = RESET;
 		end
 		
 		//fetch first instruction word
@@ -359,7 +380,7 @@ begin
 		//fetch second instruction word, skip or do MOVE instruction or halt copper
 		FETCH2:			
 		begin
-			if (!ir2[0] && illegalreg)//illegal MOVE instruction, halt copper
+			if (!ir2[0] && illegalreg) // illegal MOVE instruction, halt copper
 			begin
 				skip = 0;
 				selins = 0;
@@ -367,7 +388,7 @@ begin
 				dma_req = 0;
 				copper_next = FETCH2;
 			end
-			else if (!ir2[0] && skip_flag)//skip this MOVE instruction
+			else if (!ir2[0] && skip_flag) // skip this MOVE instruction
 			begin
 				selins = 1;
 				selreg = 0;
@@ -383,7 +404,7 @@ begin
 					copper_next = FETCH2;
 				end
 			end
-			else if(!ir2[0])//MOVE instruction
+			else if (!ir2[0]) // MOVE instruction
 			begin
 				skip = 0;
 				selins = 0;
@@ -427,13 +448,13 @@ begin
 		//WAIT or SKIP instruction
 		WAITSKIP2:
 		begin
-			if (!ir2[0])//WAIT instruction
+			if (!ir2[0]) // WAIT instruction
 			begin
-				if (beam_match)//wait is over, fetch next instruction
+				if (beam_match_wait) // wait is over, fetch next instruction
 				begin
 					skip = 0;
-					selins = 0;//both selins and selreg high --> this is a dummy cycle
-					selreg = 0;//both selins and selreg high --> this is a dummy cycle
+					selins = 0;
+					selreg = 0;
 					dma_req = 0;
 					if (dma_ena)
 						copper_next = FETCH1;
@@ -449,9 +470,9 @@ begin
 					copper_next = WAITSKIP2;
 				end
 			end
-			else//SKIP instruction
+			else // SKIP instruction
 			begin
-				if (beam_match)//compare is true, fetch next instruction and skip it if it's MOVE
+				if (beam_match_skip) // compare is true, fetch next instruction and skip it if it's MOVE
 				begin
 					skip = 1;
 					selins = 0;
