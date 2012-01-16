3b172a76bba540147efc7b55673ab3772bcba793
Rok Krajnc <rok.krajnc@gmail.com>
2012-01-16 10:08:37 +0100

    moved sw dir to fw dir.

--- rtl/de1/startram.vhd
+++ rtl/de1/startram.vhd
@@ -96,7 +96,7 @@ BEGIN
 		byte_size => 8,
 		clock_enable_input_a => "BYPASS",
 		clock_enable_output_a => "BYPASS",
-		init_file => "../../sw/menueboot/testcode.hex",
+		init_file => "../../fw/menueboot/testcode.hex",
 		intended_device_family => "Cyclone III",
 		lpm_hint => "ENABLE_RUNTIME_MOD=NO",
 		lpm_type => "altsyncram",
