e272cfc650ffd8fce74df562813ed087c80b3e28
Rok Krajnc <rok.krajnc@gmail.com>
2012-05-26 15:19:47 +0200

    sram controller changed to act as memory for OSD CPU.

--- rtl/soc/minimig_de1_top.v
+++ rtl/soc/minimig_de1_top.v
@@ -268,7 +268,6 @@ assign DRAM_BA_1    = sdram_ba[1];
 ////////////////////////////////////////
 
 /* clock */
-`define SOC_SIM
 `ifdef SOC_SIM
 // generated clocks
 initial begin
@@ -321,13 +320,14 @@ indicators indicators(
 
 /* sram controller */
 sram_ctl sram_ctl(
-  .clk          (clk_7            ),
-  .pulse        (pulse            ),
-  .fifoinptr    (fifoinptr        ),
-  .fifooutptr   (fifooutptr       ),
-  .fifowr       (fifowr           ),
-  .fifodwr      (fifodwr          ),
-  .fifodrd      (sram_fifodrd     ),
+  .clk          (clk_114          ),
+  .rst          (!n_rst           ),
+  .adr          (tg68_fast_adr    ),
+  .rw           (tg68_fast_rw     ),
+  .lds          (tg68_fast_uds    ),
+  .uds          (tg68_fast_lds    ), 
+  .dat_w        (tg68_fast_dat_out),
+  .dat_r        (/*zdataout*/         ),
   .ce           (SRAM_CE_N        ),
   .oe           (SRAM_OE_N        ),
   .wr           (SRAM_WE_N        ),
