43bd78035797f3616ec743e10eaf9165d148299c
Rok Krajnc <rok.krajnc@gmail.com>
2012-05-26 15:36:11 +0200

    added lcd simulation scripts.

---
+++ sim/lcd/nc/run/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import sys, os, copy
+
+sys.path.append("../../../common/")
+import nc_run
+import lst_parse
+import logger
+
+
+# defines
+debug         = True
+do_compile    = True
+do_elaborate  = True
+do_simulate   = True
+do_lint       = False
+do_coverage   = False
+opt_waves     = True
+opt_mems      = False
+opt_tasks     = False
+opt_funcs     = False
+top_rtl       = "lcd"
+top_sim       = "lcd_tb"
+top_cov       = "lcd"
+out_dir       = "../out"
+rtl_list      = "rtl.lst"
+lib_list      = "lib.lst"
+sim_list      = "sim.lst"
+dir_list      = "dir.lst"
+
+# run test
+nc_run.nc_run(debug=debug,
+              do_compile=do_compile, do_elaborate=do_elaborate, do_simulate=do_simulate, do_lint=do_lint, do_coverage=do_coverage,
+              opt_waves=opt_waves, opt_mems=opt_mems, opt_tasks=opt_tasks, opt_funcs=opt_funcs,
+              top_rtl=top_rtl, top_sim=top_sim, top_cov=top_cov,
+              out_dir=out_dir, rtl_list=rtl_list, lib_list=lib_list, sim_list=sim_list, dir_list=dir_list)
+
+
