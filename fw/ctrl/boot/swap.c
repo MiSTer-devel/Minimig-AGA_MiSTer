04bc5879b3c191e9d50c0f7cdad73981b560694f
Rok Krajnc <rok.krajnc@gmail.com>
2012-08-06 13:35:49 +0200

    swap files cleaned up, toggle defines added for choosing endianness, macro implementations for SwapXX functions added.

--- fw/ctrl/boot/swap.c
+++ fw/ctrl/boot/swap.c
@@ -1,15 +1,56 @@
-unsigned long SwapBBBB(unsigned long i)
+////////////////////////////////////////////////////////////////////////////////
+// swap.c                                                                     //
+// Endianness swap functions                                                  //
+//                                                                            //
+// Copyright 2005-2012                                                        //
+//                                                                            //
+// This file is part of Minimig                                               //
+//                                                                            //
+// Minimig is free software; you can redistribute it and/or modify            //
+// it under the terms of the GNU General Public License as published by       //
+// the Free Software Foundation; either version 2 of the License, or          //
+// (at your option) any later version.                                        //
+//                                                                            //
+// Minimig is distributed in the hope that it will be useful,                 //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of             //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              //
+// GNU General Public License for more details.                               //
+//                                                                            //
+// You should have received a copy of the GNU General Public License          //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.      //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+// Changelog                                                                  //
+//                                                                            //
+// 2012-08-02 - rok.krajnc@gmail.com                                          //
+// Functions are now generic - removed assembler code. Added function-like    //
+// macros (gcc specific!).                                                    //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include "swap.h"
+
+
+#ifndef SWAP_MACROS
+
+uint32_t SwapBBBB(uint32_t i)
 {
   return ((i&0x00ff0000)>>8) | ((i&0xff000000)>>24) | ((i&0x000000ff)<<24) | ((i&0x0000ff00)<<8);
 }
 
-unsigned int SwapBB(unsigned int i)
+
+uint16_t SwapBB(uint16_t i)
 {
-  return (i&0xffff0000) | ((i&0x000000ff)<<8) | ((i&0x0000ff00)>>8);
+  return ((i&0x00ff)<<8) | ((i&0xff00)>>8);
 }
 
-unsigned long SwapWW(unsigned long i)
+
+uint32_t SwapWW(uint32_t i)
 {
   //return ((i&0x0000ffff)<<16) | ((i&0xffff0000)>>16);
   return ((i<<16) | (i>>16));
 }
+
+#endif // SWAP_MACROS
+
