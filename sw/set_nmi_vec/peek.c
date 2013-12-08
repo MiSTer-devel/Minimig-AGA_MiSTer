d5d8fd25a81a9c7ccb3997347838ad0f160c9a97
Rok Krajnc <rok.krajnc@gmail.com>
2013-12-08 23:28:18 +0100

    added two small utils.

---
+++ sw/set_nmi_vec/peek.c
@@ -0,0 +1,28 @@
+/*
+// peek.c
+// 2013, rok.krajnc@gmail.com
+// Reads memory location
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char** argv)
+{
+  unsigned int adr, dat;
+
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s address\n", argv[0]);
+    exit (EXIT_FAILURE);
+  }
+
+  adr = strtoul(argv[1], 0, 0);
+
+  dat = (*(unsigned int*)(adr));
+
+  fprintf(stdout, "*(0x%08x) = 0x%08x\n", adr, dat);
+
+  exit (EXIT_SUCCESS);
+}
+
